7b97f881f0aa6e3b1349edab6f66cde4eae11689
Leo Hernandez <lhernand@ggc.edu>
2013-01-11 13:05:22 -0500

    file added

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,10 @@ public class HelloWorld {
    public static void main(String[] args)
    {
       System.out.println("Hello World");
+      System.out.println("How is your day so far?");
+      System.out.println("Remeber to smile because its FRIDAY!!!");
    }
 
 
 }
+
